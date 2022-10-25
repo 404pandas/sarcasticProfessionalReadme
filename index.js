e08f2da0e55a3eb558d8df56854e0f4e88684936
Mary Elenius <mary.panda.jackson@gmail.com>
2022-10-25 18:09:42 -0400

    Added dependencies, example prompt

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-// TODO: Include packages needed for this application
+// Inquirer command example
+// inquirer.prompt(questions, answers) -> promise
+// inquirer.registerPrompt(name, prompt)
+// inquirer.createPromptModule() -> prompt function
+
+// The Core Module File System
+const fs = require("fs");
+
+// Inquirer
+const inquirer = require("inquirer");
+
+// Jest
+const jest = require("jest");
 
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username",
+    },
+    {
+      type: "password",
+      message: "What is your password?",
+      name: "password",
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm",
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log("Success!")
+      : console.log("You forgot your password already?!")
+  );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +46,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+0;
